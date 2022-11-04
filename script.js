6c46550d746ef7acfddea4955a7cb23bdf5082d5
Shimaadnan <78430611+Shimaadnan@users.noreply.github.com>
2022-11-04 05:54:54 +0000

    level 200 woking

--- script.js
+++ script.js
@@ -5,44 +5,41 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 
 }
-const rootElem = document.getElementById("root");
 function makePageForEpisodes(episodeList) {
-  // const rootElem = document.getElementById("root");
-  const header=document.createElement('header')
-  rootElem.appendChild(header)
-  const sectionEl=document.createElement('section')
-  rootElem.appendChild(sectionEl)
-  sectionEl.setAttribute('class','main')
-  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  for(let episode of episodeList){
-  const articleEl=document.createElement("article")
-        const h3=document.createElement("h3")
-        const image=document.createElement("img")
-        let pragragh=document.createElement("p")
-        sectionEl.appendChild(articleEl);
-       articleEl.appendChild(h3);
+    const rootElem = document.getElementById("root")
+   const sectionEl=document.createElement('section')
+   sectionEl.setAttribute('class','main')
+    for(let episode of episodeList){
+        const articleEl=document.createElement("article")
+         const h3=document.createElement("h3")
+         const image=document.createElement("img")
+         let pragragh=document.createElement("p")
+         sectionEl.appendChild(articleEl);
+        articleEl.appendChild(h3);
        articleEl.appendChild(image);
        articleEl.appendChild(pragragh);
-       h3.innerText=episode.name+'-'+episode.season.toString().padStart(3,'S0')+episode.number.toString().padStart(3,'E0')
-       image.src=episode.image.medium
-       pragragh.innerHTML=episode.summary
-}
-}
-const input=document.getElementById('searchinput')
-input.addEventListener('input',matchEpisode)
-function matchEpisode(){
-
-  const allEpisodes=getAllEpisodes()
-   let matchList=allEpisodes.filter(episode=>{return (episode.name.toLowerCase().includes(input.value.toLowerCase())||episode.summary.toLowerCase().includes(input.value.toLowerCase()))})
-    let spanEl=document.getElementById('count-epispde')
-    spanEl.innerText=matchList.length +' / '+ allEpisodes.length
-    makePageForEpisodes(matchList);
+        h3.innerText=episode.name+'-'+episode.season.toString().padStart(3,'S0')+episode.number.toString().padStart(3,'E0')
+        image.src=episode.image.medium
+        pragragh.innerHTML=episode.summary
+ }
+ rootElem.innerHTML='';
+  rootElem.appendChild(sectionEl)
   
+  }
 
-  // })
+  let input=document.getElementById('searchinput')
 
-}
+  input.addEventListener('keyup',(event)=>{
+    let episodes=getAllEpisodes()
+    const searchValue=event.target.value
+     const filteredepisodes=episodes.filter(episode=>{return (episode.name.toLowerCase().includes(searchValue.toLowerCase())||episode.summary.toLowerCase().includes(searchValue.toLowerCase()))
 
+    })
+    let spanEl=document.getElementById('count-epispde')
+     spanEl.innerText=filteredepisodes.length +' / '+ episodes.length
+     makePageForEpisodes(filteredepisodes)
+
+   })
 
 
 
