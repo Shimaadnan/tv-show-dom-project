f6847e8d2c33cdc70db20ab7a7e8d953648a2514
Shimaadnan <78430611+Shimaadnan@users.noreply.github.com>
2022-11-10 11:58:24 +0000

    level 400 done

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
+const rootElem = document.getElementById("root")
+const showSelect = document.getElementById("showselection");
+const allShows = getAllShows();
+console.log(allShows[0].id);
+
+// 
 //You can edit ALL of the code here
 //Retrieve the JSON
- function getfetch(){
-fetch("https://api.tvmaze.com/shows/179/episodes")
+ function getfetch(showid){
+  let url=`https://api.tvmaze.com/shows/${showid}/episodes`
+fetch(url)
   // Get the response and extract the JSON
   .then(function (response) {
     return response.json();
   })
   // Do something with the JSON
   .then((response) => {
+    console.log(response[0])
     makePageForEpisodes(response);
+     selecttionForEpisode(response)
+    //  createShowDropDown(allShows)
      let input=document.getElementById('searchinput')
-
-  input.addEventListener('keyup',(event)=>{
+     input.addEventListener('keyup',(event)=>{
     let episodes=response
     const searchValue=event.target.value
      const filteredepisodes=episodes.filter(episode=>{return (episode.name.toLowerCase().includes(searchValue.toLowerCase())||episode.summary.toLowerCase().includes(searchValue.toLowerCase()))
@@ -22,6 +31,8 @@ fetch("https://api.tvmaze.com/shows/179/episodes")
      makePageForEpisodes(filteredepisodes)
 
    })
+  //  selecttionForEpisode(response)
+  
    const selectedepisode=document.getElementById('secletedepisodes')
   
     episodes=response
@@ -41,31 +52,25 @@ fetch("https://api.tvmaze.com/shows/179/episodes")
 
   })
 
-  })
+   })
   // If something goes wrong
   .catch((error) => console.log(error));
  }
-
- function setup() {
-   const allEpisodes = getfetch();
-    // makePageForEpisodes(allEpisodes);
-
- }
+//make page to show episodes of one show
 function makePageForEpisodes(episodeList) {
-    const rootElem = document.getElementById("root")
    const sectionEl=document.createElement('section')
    sectionEl.setAttribute('class','main')
     for(let episode of episodeList){
-        const articleEl=document.createElement("article")
-         const h3=document.createElement("h3")
-         const image=document.createElement("img")
-         let pragragh=document.createElement("p")
-         sectionEl.appendChild(articleEl);
-         articleEl.setAttribute('class','each-film')
-        articleEl.appendChild(h3);
+       const articleEl=document.createElement("article")
+       const h3=document.createElement("h3")
+       const image=document.createElement("img")
+       let pragragh=document.createElement("p")
+       sectionEl.appendChild(articleEl);
+       articleEl.setAttribute('class','each-film')
+       articleEl.appendChild(h3);
        articleEl.appendChild(image);
        articleEl.appendChild(pragragh);
-        h3.innerText=episode.name+'-'+episode.season.toString().padStart(3,'S0')+episode.number.toString().padStart(3,'E0')
+       h3.innerText=episode.name+'-'+episode.season.toString().padStart(3,'S0')+episode.number.toString().padStart(3,'E0')
         image.src=episode.image.medium
         pragragh.innerHTML=episode.summary
  }
@@ -73,38 +78,51 @@ function makePageForEpisodes(episodeList) {
    rootElem.appendChild(sectionEl)
   
    }
-
-  // let input=document.getElementById('searchinput')
-
-  // input.addEventListener('keyup',(event)=>{
-  //   let episodes=getfetch()
-  //   const searchValue=event.target.value
-  //    const filteredepisodes=episodes.filter(episode=>{return (episode.name.toLowerCase().includes(searchValue.toLowerCase())||episode.summary.toLowerCase().includes(searchValue.toLowerCase()))
-
-  //   })
-  //   let spanEl=document.getElementById('count-epispde')
-  //    spanEl.innerText=filteredepisodes.length +' / '+ episodes.length
-  //    makePageForEpisodes(filteredepisodes)
-
-  //  })
-
-  // const selectedepisode=document.getElementById('secletedepisodes')
+    // function createShowDropDown(showList) {
+  //  creates drop down select menu with each option a link to the episode
+  allShows.forEach((show) => {
+    const option = document.createElement("option"); // create option element for each ep and fill the select dropdown
+    showSelect.appendChild(option);
+    option.innerText = `${show.name}`
+    option.setAttribute('value',show.id)
+  })
+   showSelect.addEventListener('change',()=>{
+    allShows.forEach(show=>{
+      let showid=event.target.value
+      console.log(showid)
+      let url=`https://api.tvmaze.com/shows/${showid}/episodes`
+      getfetch(showid)
+    })
+   })
+//  }
+function selecttionForEpisode(){
   
-  //   episodes=getAllEpisodes()
-  // episodes.forEach(episode=>{
-  //   let optionEl=document.createElement('option')
-  //   selectedepisode.appendChild(optionEl)
-  //   optionEl.setAttribute('value',episode.id)
-  //   optionEl.innerText=episode.season.toString().padStart(3,"S0")+episode.number.toString().padStart(3,"E0")+"-"+episode.name
-  // })
-  // selectedepisode.addEventListener('change',()=>{
-  //   let episodes=getfetch()
-  //   // let e=optionEl.target.value
-  //   episodes.forEach(episode=>{if(episode.id==event.target.value){
-  //     makePageForEpisodes([episode])
-  //   }
-  // })
+}
 
-  // })
+ function setup() {
+   const allEpisodes = getAllEpisodes();
+   makePageForEpisodes(allEpisodes);
+
+ }
+
   window.onload = setup;
 
+ function selecttionForEpisode(x){
+  const selectedepisode=document.getElementById('secletedepisodes')
+  
+    let episodes=x
+  episodes.forEach(episode=>{
+    let optionEl=document.createElement('option')
+    selectedepisode.appendChild(optionEl)
+    optionEl.setAttribute('value',episode.id)
+    optionEl.innerText=episode.season.toString().padStart(3,"S0")+episode.number.toString().padStart(3,"E0")+"-"+episode.name
+  })
+  selectedepisode.addEventListener('change',()=>{
+  
+    episodes.forEach(episode=>{if(episode.id==event.target.value){
+      makePageForEpisodes([episode])
+    }
+  })
+
+  })
+}
